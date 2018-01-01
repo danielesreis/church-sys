38d24cfdb600f24d1083200f41d8bddcb94c5540
danielesreis <danielesreis@bitbucket.org>
2017-12-31 21:20:00 -0300

    working on Member class

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,8 @@
+package javaapplication2;
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        Member dani = new Member("daniele silva reis");
+        System.out.print(dani.getName());
+    }
+}
